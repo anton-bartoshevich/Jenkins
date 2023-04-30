55f20d879262811d786c6dcacb1304e91493564b
Anton Bartoshevich <anton-bartas@mail.ru>
2023-04-30 20:43:40 +0000

    Commit #3

--- seed.groovy
+++ seed.groovy
@@ -1,20 +1,17 @@
-folder('SeedJobsFolder') {
-    displayName('SeedJobsFolder')
-    description('SeedJobsFolder')
-}
-
-job("SeedJobsFolder/get__seed_job"){
-    scm {
+job("Folder"){
+   scm {
         github('anton-bartoshevich/Jenkins', 'main')
     }
-    steps {
-        shell("""
-            ls -l /var/lib/jenkins/jobs/SeedJobsFolder/jobs/
-            rm -rf /var/lib/jenkins/jobs/SeedJobsFolder/jobs/0*
+            steps {
+              shell('''
+	    ls -l /var/lib/jenkins/jobs/
+            rm -rf /var/lib/jenkins/jobs/0*
             pwd
             ls -l
-            mv /var/lib/jenkins/workspace/SeedJobsFolder/get__seed_job/jobs/* /var/lib/jenkins/jobs/SeedJobsFolder/jobs/
-            ls -l /var/lib/jenkins/jobs/SeedJobsFolder/jobs/
-        """)
+            mv /var/lib/jenkins/workspace/Folder/jobs/* '/var/lib/jenkins/jobs/'
+            ls -l /var/lib/jenkins/jobs/
+''')
+     
+        }
+
     }
-}
